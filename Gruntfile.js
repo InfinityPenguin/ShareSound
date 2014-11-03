9bc05175ac553ecb21f82e2907a1ef797001e73c
Kevin Casey <kacasey@berkeley.edu>
2014-11-03 15:37:11 -0800

    blanket coverage setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -434,10 +434,33 @@ module.exports = function (grunt) {
     },
 
     mochaTest: {
-      options: {
-        reporter: 'spec'
+      test: {
+        options: {
+          reporter: 'spec',
+          // Require blanket wrapper here to instrument other required
+          // files on the fly. 
+          //
+          // NB. We cannot require blanket directly as it
+          // detects that we are not running mocha cli and loads differently.
+          //
+          // NNB. As mocha is 'clever' enough to only run the tests once for
+          // each file the following coverage task does not actually run any
+          // tests which is why the coverage instrumentation has to be done here
+          require: 'coverage/blanket'
+        },
+        src: ['server/**/*spec.js']
       },
-      src: ['server/**/*.spec.js']
+      coverage: {
+        options: {
+          reporter: 'html-cov',
+          // use the quiet flag to suppress the mocha console output
+          quiet: true,
+          // specify a destination file to capture the mocha
+          // output (the quiet option does not suppress this)
+          captureFile: 'coverage.html'
+        },
+        src: ['server/**/*spec.js']
+      }
     },
 
     protractor: {
